64285e1bf614bdc91bfc09e9de22ec588d418b1d
Masoud Zohrabi <mdzzohrabi@gmail.com>
2014-01-20 12:29:00 +0330

    Chagne mk to md Readme file type

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Azera PHP Framework
+
+## Features
+* Auto-including system
+* Namespaced system
+* Multilingual
+* Model-View-Controller Development
+* Persian-base language
--- readme.mk
+++
@@ -1,8 +0,0 @@
-# Azera PHP Framework
-
-## Features
-* Auto-including system
-* Namespaced system
-* Multilingual
-* Model-View-Controller Development
-* Persian-base language
